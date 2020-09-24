bde2ed814f3a7d90494a6aa7d7aa83cb894bf4b0
Gerd Stolz <stolz.gerd@gmx.de>
2020-09-24 18:16:50 +0200

    started porting checkpoint_connections

--- cmk/base/plugins/agent_based/checkpoint_connections.py
+++ cmk/base/plugins/agent_based/checkpoint_connections.py
@@ -10,15 +10,79 @@
 from .agent_based_api.v1 import register, SNMPTree
 from .utils import checkpoint
 
+checkpoint_connections_default_levels = { "pct" : (80,90) }
 
-def parse_checkpoint_connections(string_table):
-    raw_value = string_table[0][0][0]
-    return {"count": int(raw_value)}
-
+def parse_checkpoint_connections(string_table: SNMPStringTable):
+    current_raw_value = string_table[0][0][0]
+    peak_raw_value = string_table[0][0][1]
+    maximum_raw_value = string_table[0][0][2]
+    return {"current": int(current_raw_value),
+            "peak": int(peak_raw_value),
+            "maximum": int(maximum_raw_value),
+            }
 
 register.snmp_section(
     name="checkpoint_connections",
     parse_function=parse_checkpoint_connections,
     detect=checkpoint.DETECT,
-    trees=[SNMPTree(base=".1.3.6.1.4.1.2620.1.1.25", oids=['3'])],
+    trees=[SNMPTree(base=".1.3.6.1.4.1.2620.1.1.25", 
+        oids=['3',  # CHECKPOINT-MIB - fwNumConn - current connections
+              '4',  # CHECKPOINT-MIB - fwPeakNumConn - peak number of connections
+              '10', # CHECKPOINT-MIB - fwConnTableLimit - connection table limit
+            ]
+        )],
 )
+def check_checkpoint_connections(section: SNMPStringTable, params: Parameters) -> CheckResult:
+    state = state.OK
+    current = section['current']
+    peak = section['peak']
+    maximum = section['maximum']
+    #infotext = "%d current, %d peak, %d maximum" % (current, peak, maximum)
+
+    if type(params) is dict and "pct" in params:
+       warn_pct, crit_pct = params["pct"]
+       warn = maximum_connections * warn_pct / 100
+       crit = maximum_connections * crit_pct / 100
+    else:
+       # up until cmk 1.6 this check only supported user set absolute values
+       warn, crit = params
+
+    yield from check_levels(
+        current,
+        levels_upper=(warn,crit)
+        metric_name="connections",
+        label=(f"Current connections: {current}, Peak: {peak}, "
+            f"Connection table limit: {maximum}" % section,)
+    )
+
+   """ 
+    if current_connections >= crit:
+        state = state.CRIT
+    elif current_connections >= warn:
+        state = state.WARN
+
+    yield Result( state = state,
+                  summary = infotext,
+                  metric
+    yield Metric(
+                name="connections",
+                value=section["count"],
+                levels=(warn, crit),
+                boundaries=(0, section["maximum"]))
+   """
+
+register.check_plugin(
+    name="checkpoint_connections",
+    service_name="Connections",
+    discovery_function=discover_checkpoint_connections,
+    check_function=check_checkpoint_connections
+    check_default_parameters=checkpoint_connections_default_levels,
+    check_ruleset_name="checkpoint_connections"
+)
+
+
+TODO:
+test - in git + with real CP
+write discovery function
+create PR
+
