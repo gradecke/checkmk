490820766afd873822878c485e88005d8aa51ea7
Gerd Stolz <stolz.gerd@gmx.de>
2020-09-25 16:11:36 +0200

    Finished testing. TODO: what is params['auto-migrated...']`

--- cmk/base/plugins/agent_based/checkpoint_connections.py
+++ cmk/base/plugins/agent_based/checkpoint_connections.py
@@ -9,7 +9,7 @@
 
 from typing import Dict
 
-from .agent_based_api.v1 import check_levels, register, Service, SNMPTree
+from .agent_based_api.v1 import check_levels, register, Service, SNMPTree, Result, state
 from .agent_based_api.v1.type_defs import CheckResult, DiscoveryResult, Parameters, SNMPStringTable
 from .utils import checkpoint
 
@@ -19,7 +19,6 @@ Section = Dict[str, int]
 
 
 def parse_checkpoint_connections(string_table: SNMPStringTable) -> Section:
-    print(string_table)
     current_raw_value = string_table[0][0][0]
     peak_raw_value = string_table[0][0][1]
     maximum_raw_value = string_table[0][0][2]
@@ -59,20 +58,24 @@ def check_checkpoint_connections(
     maximum = section['maximum']
     #infotext = "%d current, %d peak, %d maximum" % (current, peak, maximum)
 
-    if type(params) is dict and "pct" in params:
+
+    if type(params) is dict and "pct" in params and maximum > 0:
         warn_pct, crit_pct = params["pct"]
         warn = maximum * warn_pct / 100
         crit = maximum * crit_pct / 100
     else:
         # up until cmk 1.6 this check only supported user set absolute values
-        warn, crit = params
+        warn, crit = params["auto-migration-wrapper-key"]
 
+
+    upper_boundary = maximum if maximum > 0 else None
     yield from check_levels(value=current,
                             levels_upper=(warn, crit),
                             metric_name="connections",
-                            label=(f"Current connections: {current}, Peak: {peak}, "
-                                   f"Connection table limit: {maximum}" % section,))
-
+                            boundaries=(0,upper_boundary),
+                            label=("Current connections")
+                           )
+    yield Result(state=state.OK, summary="Peak: {peak}, Connection table limit: {maximum}".format(**section))
 
 register.check_plugin(name="checkpoint_connections",
                       service_name="Connections",
@@ -80,3 +83,4 @@ register.check_plugin(name="checkpoint_connections",
                       check_function=check_checkpoint_connections,
                       check_default_parameters=checkpoint_connections_default_levels,
                       check_ruleset_name="checkpoint_connections")
+
